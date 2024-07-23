0cdec3348620b4164ac59cf0401363dd0d21bf57
kgf-cmd <Karolfracek123@gmail.com>
2024-07-23 21:22:12 +0200

    manager

--- enemies/enemy_base/enemy_base.gd
+++ enemies/enemy_base/enemy_base.gd
@@ -3,8 +3,11 @@ extends CharacterBody2D
 class_name EnemyBase
 enum FACING {LEFT = -1, RIGHT = 1}
 
+const OFF_SCREEN_KILL_ME: float = 1000.0
+
 @export var default_facing: FACING = FACING.LEFT
 @export var points: int = 1
+@export var speed: float = 30.0
 
 var _speed: float = 30.0
 var _gravity: float = 800.0
@@ -18,16 +21,28 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	pass
+	fallen_off()
+	
+	
+func fallen_off() -> void:
+	if global_position.y > OFF_SCREEN_KILL_ME:
+		queue_free()
 
 
+func die():
+	if _dying == true:
+		return
+		
+	_dying = true
+	SignalManager.on_enemy_hit.emit(points,global_position)
+	set_physics_process(false)
+	hide()
+	queue_free()
 
 
+func _on_visible_on_screen_notifier_2d_screen_entered():
+	pass # Replace with function body.
 
 
-
-
-
-
-
-
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	pass # Replace with function body.
